9fb7dc314fcc9f0d239be1a8e17aa088b2618620
ashrafali-v <ashrafalifrk@gmail.com>
2025-06-21 08:43:38 +0800

    add postgres in to docker

--- libs/shared/src/index.ts
+++ libs/shared/src/index.ts
@@ -1,9 +1,18 @@
+// Entity interfaces
 export * from './lib/interfaces/product.interface';
 export * from './lib/interfaces/order.interface';
 export * from './lib/interfaces/user.interface';
+
+// Configurations and DTOs
 export * from './lib/config/rabbitmq.config';
 export * from './lib/dtos/create-order.dto';
+
+// Prisma exports
 export * from './lib/services/prisma.service';
-export * from '../generated/prisma';
-export * from './lib/services/prisma.service';
-export { PrismaClient } from './generated/prisma';
+export { 
+  PrismaClient,
+  User as PrismaUser,
+  Product as PrismaProduct,
+  Order as PrismaOrder,
+  OrderItem as PrismaOrderItem 
+} from '../generated/prisma';
